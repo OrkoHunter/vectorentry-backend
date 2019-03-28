502f7d25d5b7918b12a4123359c4770bc70e81ab
Himanshu Mishra <himanshu@orkohunter.net>
2019-03-28 16:09:39 +0530

    Bug with global, defined in the module

--- app.py
+++ app.py
@@ -9,25 +9,21 @@ import pickle
 
 app = Flask(__name__)
 
-spell_correct = None
+spell_correct = SpellingCorrectionUsingNGrams.WordSpellingCorrection('en_wikinews.txt', 'wiki_dump.pkl', 'token_cnts_wiki.json', 'word_vocab.pkl')
 
 @app.route("/")
 def main():
-    global spell_correct
-
     # with open('word_vocab.pkl', 'rb') as f:
     #     word2idx, idx2word = pickle.load(f)
     # pickle_dump(BKTree(items_dict = word2idx), 'wiki_dump.pkl')
 
     # spell_correct = SpellCorrect()
-    spell_correct = SpellingCorrectionUsingNGrams.WordSpellingCorrection('en_wikinews.txt', 'wiki_dump.pkl', 'token_cnts_wiki.json', 'word_vocab.pkl')
 
     return render_template("index.html")
 
 
 @app.route("/result")
 def result():
-    global spell_correct
 
     word = request.args.get("query")
     # Process the word
